ed5cb577e06205c64cbf7305b041ef178de2e5b4
Li <liyunwei10@gmail.com>
2020-07-29 20:55:39 -0400

    Updates

---
+++ precache-manifest.4e849d59fed34f8d2c6c67525a6ae0e1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04a6eda65c3f1255108a0d165b2d488",
+    "url": "/swiftcore/index.html"
+  },
+  {
+    "revision": "8265dc41e4e9276802f6",
+    "url": "/swiftcore/static/css/main.04ec84b2.chunk.css"
+  },
+  {
+    "revision": "4ce5ef770f1834779917",
+    "url": "/swiftcore/static/js/2.539db5ed.chunk.js"
+  },
+  {
+    "revision": "43190b42fefe83e12b28e609399bf024",
+    "url": "/swiftcore/static/js/2.539db5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8265dc41e4e9276802f6",
+    "url": "/swiftcore/static/js/main.ba3e17d0.chunk.js"
+  },
+  {
+    "revision": "9d365b812f6e856822a1",
+    "url": "/swiftcore/static/js/runtime-main.f5cb67c1.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/swiftcore/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/swiftcore/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/swiftcore/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "a6326f736fea5427787068e988f61d45",
+    "url": "/swiftcore/static/media/logo.a6326f73.svg"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/swiftcore/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "a39c103b5ec972caec85b2f7637ef297",
+    "url": "/swiftcore/static/media/nav-background.a39c103b.jpg"
+  },
+  {
+    "revision": "9a9cab82ac0c8df725576a0dd54d9520",
+    "url": "/swiftcore/static/media/pic1.9a9cab82.jpg"
+  },
+  {
+    "revision": "380d7419b451b61cb0ea1b494cb2efb0",
+    "url": "/swiftcore/static/media/pic2.380d7419.jpg"
+  },
+  {
+    "revision": "ccf1f1413ccd2f1c77c46296ab5402e3",
+    "url": "/swiftcore/static/media/undraw_data_trends_b0wg-1.ccf1f141.svg"
+  },
+  {
+    "revision": "18f79814f5c27856194591e5158cb9fa",
+    "url": "/swiftcore/static/media/up-arrow-circle.18f79814.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/swiftcore/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
